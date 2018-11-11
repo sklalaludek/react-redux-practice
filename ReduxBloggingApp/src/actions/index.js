0c8bf33829d52269f47ce290b36c8ec4c837c87a
sklalaludek <pattiegolis@gmail.com>
2018-11-11 23:23:42 +0100

    add post new and post show

--- ReduxBloggingApp/src/actions/index.js
+++ ReduxBloggingApp/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
+export const CREATE_POST = 'create_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com';
 const API_KEY = '?key=MANYCATS123';
@@ -10,6 +12,25 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: req
+  };
+}
+
+export function createPost(values, callback) {
+  const req = axios.post(`${ROOT_URL}/api/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: req
+  }
+}
+
+export function fetchPost(id) {
+  const req = axios.get(`${ROOT_URL}/api/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: req
   };
 }
--- ReduxBloggingApp/src/components/posts_index.js
+++ ReduxBloggingApp/src/components/posts_index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -9,19 +10,25 @@ class PostsIndex extends Component {
   }
 
   renderPosts() {
-    _.map(this.props.posts, post => {
+    return _.map(this.props.posts, post => {
       return (
         <li className="list-group-item" key={post.id}>
-          {post.title}
+          <Link to={`/posts/${post.id}`}>
+            {post.title}
+          </Link>
         </li>
       )
     });
   }
 
   render() {
-    console.log(this.props.posts)
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
